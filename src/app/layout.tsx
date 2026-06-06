c33e4582b116d0fcbfca513eeb96c7f55ba042de
BarryTheAllen <BarryTheAllen@users.noreply.github.com>
2026-06-06 17:20:19 +0400

    asdasdasdwer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/Header/Header";
-import Providers from "./providers";
+import Providers from "@/lib/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
